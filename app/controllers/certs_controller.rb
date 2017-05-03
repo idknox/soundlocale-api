6198f0cbebc8d086bf739a1607636840e62f36bb
Ian Knox <knoxid@gmail.com>
2017-05-03 13:48:00 -0600

    remove cert endpoint

--- app/controllers/certs_controller.rb
+++
@@ -1,5 +0,0 @@
-class CertsController < ApplicationController
-  def letsencrypt
-    render plain: ENV['LETSENCRYPT_KEY']
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'api/sessions', registrations: 'api/registrations'}
 
   root to: 'api/sessions#new'
-  get '/.well-known/acme-challenge/:id' => 'certs#letsencrypt'
 
   namespace :api do
     get 'me', to: 'users#me'
